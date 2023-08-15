911d8fe2cdc79f787ac8a5bbcb78698f68144317
Lars Johansson <lars.johansson@gmail.com>
2023-08-15 15:21:24 +0200

    Added cognitive search

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
+import os
 from fastapi import FastAPI
 from routers.wellknown import wellknown
 from fastapi.middleware.cors import CORSMiddleware
+from azure.core.credentials import AzureKeyCredential
+from azure.search.documents import SearchClient
 
 app = FastAPI()
 app.include_router(wellknown)
 app.add_middleware(CORSMiddleware, allow_origins=["https://chat.openai.com"])
 
 
-@app.get("/role", summary="Get a role to play", operation_id="getRole")
-async def get_role():
+@app.get("/info", summary="Get information from Azure cognitive search by extracting a query from the ", operation_id="getInfo")
+async def get_info(query: str = None):
     """
-    Returns a role to play
+    Returns information based on a query parameter.
     """
-    return "You are an elite biology professor."
+
+    index_name = "index3"
+    # Get the service endpoint and API key from the environment
+    endpoint = os.environ["search-endpoint"]
+    key = os.environ["search-key"]
+
+    # Create a client
+    credential = AzureKeyCredential(key)
+    client = SearchClient(endpoint=endpoint,
+                        index_name=index_name,
+                        credential=credential)
+    
+    results = client.search(search_text=query)
+    return [
+        results
+    ]
+
